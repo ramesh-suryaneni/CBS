e05360897d3df79bca08956056637f3f1c38e6be
7dayz-ramesh <ramesh.suryaneni@yash.com>
2020-03-03 12:38:49 +0530

    initial setup
    initial setup

---
+++ src/main/java/com/imagination/cbs/ContractorBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.imagination.cbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractorBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractorBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imagination/cbs/ContractorbookingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imagination.cbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractorBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
