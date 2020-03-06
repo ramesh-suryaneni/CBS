3d5e8d4f84f9590a41689e34ae98768a1ebd746e
PappuRout <pappuroutvtu@gmail.com>
2020-03-06 12:30:28 +0530

    Rremoved unused import

--- src/test/java/com/imagination/cbs/ContractorbookingsystemApplicationTests.java
+++ src/test/java/com/imagination/cbs/ContractorbookingsystemApplicationTests.java
@@ -1,12 +1,12 @@
 package com.imagination.cbs;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ContractorBookingSystemApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
