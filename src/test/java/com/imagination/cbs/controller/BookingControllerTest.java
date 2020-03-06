ec3a8155565bc603dd29750f612958b620a065c3
Monali Balaji Jadhav <monali.jadhav@yash.com>
2020-03-06 14:27:20 +0530

    test

---
+++ src/test/java/com/imagination/cbs/controller/BookingControllerTest.java
@@ -0,0 +1,37 @@
+package com.imagination.cbs.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(BookingController.class)
+public class BookingControllerTest {
+
+	/*@Autowired
+	private MockMvc mvc;
+
+	@MockBean
+	private BookingService bookingServiceImpl;
+	
+	@Test
+	public void shouldGetAllDesciplines(){
+		
+	 List<DisciplineDto> listofDisciplineDto= new ArrayList<>();
+	 
+		
+		
+		
+	}*/
+	
+	
+	/*public DisciplineDto getDiscipline(){
+		
+		DisciplineDto disciplineDto= new DisciplineDto();
+		
+		
+		
+		
+	}*/
+
+}
