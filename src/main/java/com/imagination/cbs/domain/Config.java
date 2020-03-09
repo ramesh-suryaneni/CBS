7292c6f2f310cacfec13946998ca5e29a4941c98
sanvaliya.patidar <sanvaliya.patidar@YP1010737DT.Yash.com>
2020-03-09 18:45:26 +0530

    Made changes for Adobe OAuth Token.

--- src/main/java/com/imagination/cbs/domain/Config.java
+++ src/main/java/com/imagination/cbs/domain/Config.java
@@ -15,6 +15,7 @@ public class Config implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="config_id")
 	private long configId;
 
