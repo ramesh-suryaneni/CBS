5e3057acb68a8cb9cb9c735c5afeddd4e6126c32
aashoo.rajput <aashoo.rajput@YP1010071DT.Yash.com>
2020-03-12 17:08:13 +0530

    Rest API for Country and Offices

--- src/main/java/com/imagination/cbs/dto/DisciplineDto.java
+++ src/main/java/com/imagination/cbs/dto/DisciplineDto.java
@@ -1,22 +1,12 @@
 package com.imagination.cbs.dto;
 
 import lombok.Data;
-/*import lombok.Setter;*/
-import lombok.Getter;
-import lombok.Setter;
-
 @Data
 public class DisciplineDto {
-	
-	@Setter
-	@Getter
+
 	private long disciplineId;
 
-	@Setter
-	@Getter
 	private String disciplineDescription;
 
-	@Setter
-	@Getter
 	private String disciplineName;
 }
