572730e4533714a2997e63564e9839fcd818cdbf
sanvaliya.patidar <sanvaliya.patidar@YP1010737DT.Yash.com>
2020-03-13 19:15:32 +0530

    Resolved conflicts.

--- src/main/java/com/imagination/cbs/controller/ContractorController.java
+++ src/main/java/com/imagination/cbs/controller/ContractorController.java
@@ -3,18 +3,9 @@
  */
 package com.imagination.cbs.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.imagination.cbs.service.impl.AdobeOAuthTokensServiceImpl;
 
 /**
  * @author Ramesh.Suryaneni
@@ -25,23 +16,4 @@ import com.imagination.cbs.service.impl.AdobeOAuthTokensServiceImpl;
 @RequestMapping(value = "/contractors")
 public class ContractorController {
 
-	@Autowired
-	AdobeOAuthTokensServiceImpl accessToken;
-	
-	@GetMapping("/getAdobeOauthAccessToken")
-	public ResponseEntity<Object> getAdobeOauthAccessToken() {
-		String adobeOAuthToken = null;
-		Map<String,Object> map=new HashMap<>();
-
-		try {
-			adobeOAuthToken = accessToken.getOauthAccessToken();
-			map.put("accessToken", adobeOAuthToken);
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new ResponseEntity<>(HttpStatus.FORBIDDEN);
-		}
-		return new ResponseEntity<Object>(map, HttpStatus.OK);
-	}
-	
 }
