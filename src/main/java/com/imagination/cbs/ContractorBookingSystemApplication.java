a0ba8da1b917af67040b02e66586c01546947674
sanvaliya.patidar <sanvaliya.patidar@YP1010737DT.Yash.com>
2020-03-16 20:14:36 +0530

    remove unused code.

--- src/main/java/com/imagination/cbs/ContractorBookingSystemApplication.java
+++ src/main/java/com/imagination/cbs/ContractorBookingSystemApplication.java
@@ -1,12 +1,9 @@
 package com.imagination.cbs;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
-import com.imagination.cbs.service.impl.AdobeOAuthTokensServiceImpl;
 
 @SpringBootApplication
 public class ContractorBookingSystemApplication {
@@ -15,10 +12,7 @@ public class ContractorBookingSystemApplication {
 
 		SpringApplication.run(ContractorBookingSystemApplication.class, args);
 	}
-
-	@Autowired
-	AdobeOAuthTokensServiceImpl adobe;
-
+	
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
