4ea9d7f71cf948e634e9a61cfa3b6b0bc83dd70f
ramesh.suryaneni <ramesh.suryaneni@gmail.com>
2020-03-16 20:40:47 +0530

    exception handling classes re-added to fix build errors.

---
+++ src/main/java/com/imagination/cbs/exception/CBSApplicationException.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.imagination.cbs.exception;
+
+/**
+ * @author Ramesh.Suryaneni
+ *
+ */
+public class CBSApplicationException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String errorMessage;
+
+	@Override
+	public String getMessage() {
+		return errorMessage;
+	}
+
+	public CBSApplicationException() {
+		super();
+	}
+
+	public CBSApplicationException(String errorMessage) {
+		super(errorMessage);
+		this.errorMessage = errorMessage;
+	}
+
+}
--- src/main/java/com/imagination/cbs/exception/CBSGlobalExceptionHandler.java
+++ src/main/java/com/imagination/cbs/exception/CBSGlobalExceptionHandler.java
@@ -3,7 +3,18 @@
  */
 package com.imagination.cbs.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.imagination.cbs.dto.ErrorResponse;
 
 /**
  * @author Ramesh.Suryaneni
@@ -12,6 +23,27 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class CBSGlobalExceptionHandler {
+	
+	@ExceptionHandler(CBSApplicationException.class)
+	public ResponseEntity<ErrorResponse> exceptionHandler(Exception ex) {
+		ErrorResponse error = new ErrorResponse();
+		error.setErrorCode(HttpStatus.PRECONDITION_FAILED.value());
+		error.setMessage(ex.getMessage());
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Map<String, String> handleValidationException(MethodArgumentNotValidException ex) {
+		Map<String, String> errors = new HashMap<>();
+		ex.getBindingResult().getAllErrors().forEach(error -> {
+			String fieldName = ((FieldError) error).getField();
+			String errMsg = error.getDefaultMessage();
+			errors.put(fieldName, errMsg);
+		});
+
+		return errors;
+	}
 	
 
 }
