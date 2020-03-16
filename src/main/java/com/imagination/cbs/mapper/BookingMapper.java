5ae7317dbd03d012610875cf27febaa410f97480
aashoo.rajput <aashoo.rajput@YP1010071DT.Yash.com>
2020-03-16 18:26:55 +0530

    Remove unused code.

--- src/main/java/com/imagination/cbs/mapper/BookingMapper.java
+++ src/main/java/com/imagination/cbs/mapper/BookingMapper.java
@@ -3,7 +3,6 @@ package com.imagination.cbs.mapper;
 import java.util.List;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 import com.imagination.cbs.domain.Discipline;
 import com.imagination.cbs.domain.RoleDm;
@@ -13,13 +12,8 @@ import com.imagination.cbs.dto.DisciplineDto;
 @Mapper(componentModel = "spring")
 public interface BookingMapper {
 
-	BookingMapper INSTANCE = Mappers.getMapper( BookingMapper.class );
-	
-	//public DisciplineDto toDisciplineDTO(Discipline discipline);
-
 	public List<DisciplineDto> toListDisciplineDTO(List<Discipline> listOfDiscipline);
-	
+
 	public List<ContractorRoleDto> toListContractorRoleDto(List<RoleDm> listOfRoles);
-	
-	
+
 }
