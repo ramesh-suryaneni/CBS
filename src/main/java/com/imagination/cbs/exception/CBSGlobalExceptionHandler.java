1ab3bdb53f4a0ac1319073932f68b743f5e9b423
Dhananjay Shelke <dhananjay.shelke@yash.com>
2020-03-18 19:09:48 +0530

    resolved merge conflict

--- src/main/java/com/imagination/cbs/exception/CBSGlobalExceptionHandler.java
+++ src/main/java/com/imagination/cbs/exception/CBSGlobalExceptionHandler.java
@@ -45,11 +45,13 @@ public class CBSGlobalExceptionHandler {
 		return errors;
 	}
 	
+
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<CBSErrorMessage> handleResourceNotFoundException(RuntimeException runtimeException) {
 		CBSErrorMessage cbsErrorMessage =  new CBSErrorMessage();
 		cbsErrorMessage.setErrorMessage(runtimeException.getMessage());
 		return new ResponseEntity<>(cbsErrorMessage, HttpStatus.NOT_FOUND);
+
 	}
 	
 
