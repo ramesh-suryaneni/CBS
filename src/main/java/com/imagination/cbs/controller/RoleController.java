d93fad16be56092b3bcb1efee6a64324a1f29dc2
ramesh.suryaneni <ramesh.suryaneni@gmail.com>
2020-03-18 16:40:24 +0530

    cbs-17:cest outcome path variable issue fixed.

--- src/main/java/com/imagination/cbs/controller/RoleController.java
+++ src/main/java/com/imagination/cbs/controller/RoleController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -29,7 +30,7 @@ public class RoleController {
 	RoleService roleService;
 	
 	@GetMapping("/{role_id}/cestoutcome")
-	public ContractorRoleDto getRoleCESToutcome(Long roleId) {
+	public ContractorRoleDto getRoleCESToutcome(@PathVariable("role_id") Long roleId) {
 		
 		if(roleService.getCESToutcome(roleId) == null)
 			throw new ResponseStatusException(
