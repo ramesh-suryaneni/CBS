a8e56c240dd25381703d7e9c6325997f4feae4e3
Dhananjay Shelke <dhananjay.shelke@yash.com>
2020-03-19 15:13:33 +0530

    token verification changes

--- src/main/java/com/imagination/cbs/config/SpringSecurityConfig.java
+++ src/main/java/com/imagination/cbs/config/SpringSecurityConfig.java
@@ -5,6 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,60 +19,54 @@ import com.imagination.cbs.security.GoogleIDTokenValidationFilter;
 
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	private Logger logger=LoggerFactory.getLogger(SpringSecurityConfig.class);
-	
+
+	private Logger logger = LoggerFactory.getLogger(SpringSecurityConfig.class);
+
 	@Value("${cbssecurity.enabled:true}")
 	private boolean securityEnabled;
-	
+
 	@Autowired
 	private GoogleIDTokenValidationFilter googleIDTokenValidationFilter;
-	
+
 	@Autowired
 	private GoogleAuthenticationEntryPoint unauthorizedHandler;
 	
-	   @Override
-	    public void configure(WebSecurity web) throws Exception {
-	      web
-	        .ignoring()
-	        .antMatchers("/token/*", 
-            		"/v2/**",
-                    "/configuration/**",
-                    "/migration/**",
-                    "/swagger-resources/**",
-                    "/swagger-ui.html",
-                    "/webjars/**");
-	    }
+	@Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
-	    @Override
-	    protected void configure(HttpSecurity http) throws Exception {
-	    	
-	    	logger.info("SecurityEnabled" + securityEnabled);
-	    	
-	    	if(securityEnabled){
-	    		logger.info("Security enabled");
-	    		http.cors().and().csrf().disable().antMatcher("/**").
-                authorizeRequests().antMatchers("/", "/index.html","/token/*", 
-                		"/v2/**",
-                        "/configuration/**",
-                        "/migration/**",
-                        "/swagger-resources/**",
-                        "/swagger-ui.html",
-                        "/webjars/**").permitAll()
-                
-                //** BOTH ADMIN/USER CAN ACCESS **//*
-                //** ONLY ADMIN CAN ACCESS **//*
-                .anyRequest().authenticated().and().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).
-                and().addFilterBefore(googleIDTokenValidationFilter,UsernamePasswordAuthenticationFilter.class);
-	    	}else{
-	    		logger.info("Security Disabled");
-	    		
-	    		http.cors().and().csrf().disable().authorizeRequests().antMatchers("/**").permitAll();
-	    	}
-	        
-	                
-	    }
-	    
-	  
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/token/*", "/v2/**", "/configuration/**", "/migration/**", "/swagger-resources/**",
+				"/swagger-ui.html", "/webjars/**");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		logger.info("SecurityEnabled" + securityEnabled);
+
+		if (securityEnabled) {
+			
+			logger.info("Security enabled");
+			
+			http.cors().and().csrf().disable();
+			
+			http.authorizeRequests()
+					.anyRequest().authenticated().and().exceptionHandling()
+					.authenticationEntryPoint(unauthorizedHandler).and()
+					.addFilterAfter(googleIDTokenValidationFilter, UsernamePasswordAuthenticationFilter.class);
+		
+		} else {
+			
+			logger.info("Security Disabled");
+
+			http.cors().and().csrf().disable().authorizeRequests().antMatchers("/**").permitAll();
+		}
+
+	}
 
 }
