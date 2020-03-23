c7e84ddf84b42f2e2aeb5e1b2027e806fabffd15
PappuRout <pappuroutvtu@gmail.com>
2020-03-23 13:28:42 +0530

    Added Coross Origin

--- src/main/java/com/imagination/cbs/controller/MaconomyController.java
+++ src/main/java/com/imagination/cbs/controller/MaconomyController.java
@@ -4,6 +4,7 @@
 package com.imagination.cbs.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,7 +17,7 @@ import com.imagination.cbs.service.MaconomyService;
  * @author pappu.rout
  *
  */
-
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/macanomy")
 public class MaconomyController {
