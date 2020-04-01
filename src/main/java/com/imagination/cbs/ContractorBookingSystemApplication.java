7e8fc5322bbbd3366f1ee4ad76e57c13a1688e4a
PappuRout <pappuroutvtu@gmail.com>
2020-04-01 15:25:31 +0530

    Removed unuse code.

--- src/main/java/com/imagination/cbs/ContractorBookingSystemApplication.java
+++ src/main/java/com/imagination/cbs/ContractorBookingSystemApplication.java
@@ -2,13 +2,9 @@ package com.imagination.cbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
-
-import com.imagination.cbs.security.GoogleIDTokenValidationFilter;
 
 @SpringBootApplication
 @EnableCaching
@@ -24,15 +20,4 @@ public class ContractorBookingSystemApplication {
 		return new RestTemplate();
 	}
 	
-	// this removes filter in case of local profile
-	@Bean
-	@Profile("local")
-	public FilterRegistrationBean<GoogleIDTokenValidationFilter> registration(GoogleIDTokenValidationFilter filter) {
-	    
-		FilterRegistrationBean<GoogleIDTokenValidationFilter> registration = new FilterRegistrationBean<GoogleIDTokenValidationFilter>(filter);
-	    registration.setEnabled(false);
-	   
-	    
-	    return registration;
-	}
 }
