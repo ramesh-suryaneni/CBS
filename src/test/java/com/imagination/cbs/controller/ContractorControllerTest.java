9d35593333a341717902c72daa98ef4d2bf59130
aashoo-rajput <aashoo.rajput@yash.com>
2020-04-02 18:25:43 +0530

    Added Test in ContractorControllerTest

--- src/test/java/com/imagination/cbs/controller/ContractorControllerTest.java
+++ src/test/java/com/imagination/cbs/controller/ContractorControllerTest.java
@@ -1,31 +1,40 @@
 package com.imagination.cbs.controller;
 
 import static org.hamcrest.Matchers.comparesEqualTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
-
+import org.apache.http.HttpStatus;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.imagination.cbs.dto.ContractorDto;
 import com.imagination.cbs.dto.ContractorEmployeeDto;
+import com.imagination.cbs.dto.ContractorEmployeeRequest;
+import com.imagination.cbs.dto.ContractorRequest;
 import com.imagination.cbs.service.ContractorService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -40,6 +49,10 @@ public class ContractorControllerTest {
 
 	@MockBean
 	private ContractorService contractorService;
+	
+	@Autowired
+	ObjectMapper objectMapper;
+	
 
 	@Before
     public void setup() {
@@ -75,38 +88,82 @@ public class ContractorControllerTest {
 		verify(contractorService).getContractorEmployeeByContractorIdAndEmployeeId(6000l, 5000l);
 	}
 	
-/*	@Test
-	public void shouldReturnListOfContractorsByContractorName() throws Exception {
-		
-		List<ContractorDto> contractorDtos = getContractorDtos();
-
-		when(contractorService.getContractorDeatilsContainingName("Im")).thenReturn(contractorDtos);
-
-		mockMvc.perform(get("/contractors/Im").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-				.andExpect(jsonPath("$[0].contractorName", comparesEqualTo("Imagination")));
-
-		verify(contractorService).getContractorsByContractorName("Im");
-	}
-
+	@WithMockUser("developer")
 	@Test
-	public void shouldReturnEmptyContractListIfContractorNameIsNotPresentInDB() throws Exception {
-		List<ContractorDto> contractorDtos = new ArrayList<>();
+	public void shouldAddNewContractorEmployee() throws Exception
+	{
+		String jsonRequest = objectMapper.writeValueAsString(createContractorEmployeeRequest());
+		
+		when(contractorService.addContractorEmployee(Mockito.anyLong(), Mockito.any(ContractorEmployeeRequest.class)))
+				.thenReturn(createContractorEmployeeDto());
+		
+		MvcResult mvcResult = this.mockMvc.perform(post("/contractors/6000/employees")
+				.content(jsonRequest)
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isCreated())
+				.andReturn();
 
-		when(contractorService.getContractorsByContractorName(Mockito.anyString())).thenReturn(contractorDtos);
-
-		mockMvc.perform(get("/contractors/Test").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-				.andExpect(jsonPath("$").isEmpty());
-
-		verify(contractorService).getContractorsByContractorName("Test");
+		assertEquals(HttpStatus.SC_CREATED, mvcResult.getResponse().getStatus());
+		
 	}
-*/
+	
+	@WithMockUser("devloper")
+	@Test
+	public void shouldAddNewContractor() throws Exception
+	{
+		String jsonRequest = objectMapper.writeValueAsString(createContractorRequest());
+		
+		when(contractorService.addContractorDetails(Mockito.any(ContractorRequest.class)))
+			.thenReturn(createContractorDto());
+		
+		MvcResult mvcResult = this.mockMvc.perform(post("/contractors")
+					.content(jsonRequest)
+					.contentType(MediaType.APPLICATION_JSON))
+					.andExpect(status().isCreated())
+					.andReturn();
+		assertEquals(HttpStatus.SC_CREATED, mvcResult.getResponse().getStatus());
+	}
+	
+	@WithMockUser("developer")
+	@Test
+	public void shouldReturnPageResponseOfContractorDtoByContractorName() throws Exception {
+		
+		when(contractorService.getContractorDeatilsContainingName("Imagination",0,10,"contractorName","ASC")).thenReturn(createPageData());
+
+		this.mockMvc.perform(get("/contractors?name=Imagination").contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.content[0].contractorId").value(101));
+
+		verify(contractorService).getContractorDeatilsContainingName("Imagination",0,10,"contractorName","ASC");
+	}
+
+	@WithMockUser("developer")
+	@Test
+	public void shouldReturnPageResponseOfContractorDtoIfContractorNameIsNotPresentInDB() throws Exception {
+		
+		when(contractorService.getContractorDeatils(0, 10, "contractorName", "ASC")).thenReturn(createPageData());
+		
+		this.mockMvc.perform(get("/contractors").contentType(MediaType.APPLICATION_JSON))
+					.andExpect(status().isOk())
+					.andExpect(jsonPath("$.content[0].contractorId").value(101))
+					.andExpect(jsonPath("$.content[1].contractorId").value(201));
+		
+		verify(contractorService).getContractorDeatils(0, 10, "contractorName", "ASC");
+	}
+
 	public List<ContractorDto> getContractorDtos() {
+		
 		List<ContractorDto> contractorDtoList = new ArrayList<>();
+		
 		ContractorDto contractorDto1 = new ContractorDto();
 		contractorDto1.setContractorId(101);
 		contractorDto1.setContractorName("Imagination");
+		ContractorDto contractorDto2  = new ContractorDto();
+		contractorDto2.setContractorId(201);
+		contractorDto2.setContractorName("Stable Films Pty. LTD. (Marni Baker)");
+		
 		contractorDtoList.add(contractorDto1);
-
+		contractorDtoList.add(contractorDto2);
 		return contractorDtoList;
 	}
 	
@@ -128,5 +185,32 @@ public class ContractorControllerTest {
 		contractorEmployeeDto.setKnownAs("Aliase1");
 		
 		return contractorEmployeeDto;
+	}
+	
+	public ContractorEmployeeRequest createContractorEmployeeRequest()
+	{
+		ContractorEmployeeRequest contractorEmpRequest = new ContractorEmployeeRequest();
+		contractorEmpRequest.setContractorEmployeeName("Akshay");
+		contractorEmpRequest.setCurrencyId(203l);
+		contractorEmpRequest.setDayRate(new BigDecimal(342));
+		contractorEmpRequest.setKnownAs("Aliace3");
+		contractorEmpRequest.setRoleId(123l);
+		
+		return contractorEmpRequest;
+	}
+	
+	public ContractorRequest createContractorRequest()
+	{
+		ContractorRequest contractorRequest = new ContractorRequest();
+		contractorRequest.setContactDetails("Details5");
+		contractorRequest.setContractorName("CBS");
+		contractorRequest.setServiceProvided("Yash");
+		
+		return contractorRequest;
+	}
+	
+	public Page<ContractorDto> createPageData()
+	{
+		return new PageImpl<>(getContractorDtos(),PageRequest.of(0, 10),2);
 	}
 }
