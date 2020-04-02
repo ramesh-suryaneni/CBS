ded0a1aa1e9ce336b559cf464bbb93a0f4703a51
Akshay Dnyaneshwar Hande <akshay.hande@yash.com>
2020-04-02 14:46:21 +0530

    Create contractor and create ContractorEmployee workflow.

--- src/main/java/com/imagination/cbs/mapper/ContractorMapper.java
+++ src/main/java/com/imagination/cbs/mapper/ContractorMapper.java
@@ -3,9 +3,11 @@ package com.imagination.cbs.mapper;
 import java.util.List;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.imagination.cbs.domain.Contractor;
 import com.imagination.cbs.dto.ContractorDto;
+import com.imagination.cbs.dto.ContractorRequest;
 
 @Mapper(componentModel = "spring")
 public interface ContractorMapper {
@@ -14,4 +16,6 @@ public interface ContractorMapper {
 
 	public ContractorDto toContractorDtoFromContractorDomain(Contractor contractor);
 
+	@Mapping(source = "serviceProvided", target = "companyType")
+	public Contractor toContractorDomainFromContractorRequest(ContractorRequest contractorRequest);
 }
