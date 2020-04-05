9f53a37e936d126fae1daee74a2e4f8d55898d64
PappuRout <pappuroutvtu@gmail.com>
2020-04-05 14:07:00 +0530

    changed booking repository.

--- src/main/java/com/imagination/cbs/repository/BookingRevisionRepository.java
+++ src/main/java/com/imagination/cbs/repository/BookingRevisionRepository.java
@@ -19,13 +19,13 @@ public interface BookingRevisionRepository extends JpaRepository<BookingRevision
 	
 	public static final String BOOKING_REVISIONQU_DETAILS_QUERY = "SELECT asd.approval_name as status, br.booking_id as bookingId, br.job_name as jobName, rd.role_name as roleName, br.contractor_name as contractorName,"
 			+ " br.contracted_from_date as contractedFromDate, br.contracted_to_date as contractedToDate, br.changed_by as changedBy, br.changed_date as changedDate"
-			+ " FROM cbs.approval_status_dm asd, cbs.contractor_employee_role cer , cbs.role_dm rd,"
+			+ " FROM cbs.approval_status_dm asd, cbs.role_dm rd,"
 			+ " (Select booking_id, Max(revision_number) as maxRevision from cbs.booking_revision WHERE changed_by = :loggedInUser";
 
 	@Query(value = BOOKING_REVISIONQU_DETAILS_QUERY
 			+ " and approval_status_id = (SELECT approval_status_id from cbs.approval_status_dm where approval_name = :status)"
 			+ " group by booking_id) as irn INNER JOIN cbs.booking_revision br on br.booking_id=irn.booking_id and br.revision_number=irn.maxRevision"
-			+ " WHERE br.contractor_employee_role_id = cer.contractor_employee_role_id and cer.role_id = rd.role_id and br.approval_status_id = asd.approval_status_id", nativeQuery = true)
+			+ " WHERE br.role_id = rd.role_id and br.approval_status_id = asd.approval_status_id", nativeQuery = true)
 	public List<Tuple> retrieveBookingRevisionForDraftOrCancelled(@Param("loggedInUser") String loggedInUser,
 			@Param("status") String status, Pageable pageable);
 
@@ -36,7 +36,7 @@ public interface BookingRevisionRepository extends JpaRepository<BookingRevision
 	@Query(value = BOOKING_REVISIONQU_DETAILS_QUERY
 			+ " and approval_status_id in (SELECT approval_status_id from cbs.approval_status_dm where approval_name != 'Draft' AND approval_name != 'Cancelled')"
 			+ " group by booking_id) as irn INNER JOIN cbs.booking_revision br on br.booking_id=irn.booking_id and br.revision_number=irn.maxRevision"
-			+ " WHERE br.contractor_employee_role_id = cer.contractor_employee_role_id and cer.role_id = rd.role_id and br.approval_status_id = asd.approval_status_id", nativeQuery = true)
+			+ " WHERE br.role_id = rd.role_id and br.approval_status_id = asd.approval_status_id", nativeQuery = true)
 	public List<Tuple> retrieveBookingRevisionForSubmitted(@Param("loggedInUser") String loggedInUser, Pageable pageable);
 	
 	@Query(value = "SELECT asd.approval_name as status, br.booking_id as bookingId, br.job_name as jobName, rd.role_name as roleName, br.contractor_name as contractorName,"
