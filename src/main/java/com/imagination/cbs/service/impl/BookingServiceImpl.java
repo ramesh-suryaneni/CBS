23cb48798cc538f6a1d8156b26963c49f65f1698
Pravin Naganath Budage <pravin.budage@Yash.com>
2020-04-07 17:11:16 +0530

    Fixed minor issue

--- src/main/java/com/imagination/cbs/service/impl/BookingServiceImpl.java
+++ src/main/java/com/imagination/cbs/service/impl/BookingServiceImpl.java
@@ -12,10 +12,9 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import com.imagination.cbs.constant.ApprovalStatusConstant;
