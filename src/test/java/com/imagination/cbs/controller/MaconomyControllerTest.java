22ee8bc85701edbd966cf86556434b2d5d6214a2
PappuRout <pappuroutvtu@gmail.com>
2020-04-10 18:38:26 +0530

    Make changes in MacanomtController test class

--- src/test/java/com/imagination/cbs/controller/MaconomyControllerTest.java
+++ src/test/java/com/imagination/cbs/controller/MaconomyControllerTest.java
@@ -1,4 +1,4 @@
-/*package com.imagination.cbs.controller;
+package com.imagination.cbs.controller;
 
 import static org.hamcrest.Matchers.comparesEqualTo;
 import static org.mockito.Mockito.when;
@@ -6,23 +6,18 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.MediaType;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import com.imagination.cbs.config.TestConfig;
 import com.imagination.cbs.dto.JobDataDto;
@@ -45,18 +40,20 @@ public class MaconomyControllerTest {
 	@MockBean
 	private RestTemplateBuilder restTemplateBuilder;
 	
+	@MockBean
+	private MaconomyService maconomyService;
+	
 	@Autowired
 	private MockMvc mockMvc;
 	
-	@MockBean
-	private MaconomyService maconomyService;
 	
 	@WithMockUser("developer")
 	@Test
 	public void shouldReturnJobDataDtoBasedOnJobNumber() throws Exception
 	{
-		String jobNo="6000";
-		when(maconomyService.getJobDetails(jobNo)).thenReturn(createJobDataDto());
+		
+		
+		when(maconomyService.getMaconomyJobNumberAndDepartmentsDetails(Mockito.anyString(), Mockito.any(JobDataDto.class), Mockito.anyString(), Mockito.anyString())).thenReturn(createJobDataDto());
 		
 		this.mockMvc.perform(get("/macanomy?jobNumber=6000").accept(MediaType.APPLICATION_JSON))
 					.andExpect(status().isOk())
@@ -82,4 +79,3 @@ public class MaconomyControllerTest {
 	}
 
 }
-*/
