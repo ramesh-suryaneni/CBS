b9d35f48076380563c2fc6d18a20444c8eed0005
PappuRout <pappuroutvtu@gmail.com>
2020-04-25 16:55:28 +0530

    added test

--- src/test/java/com/imagination/cbs/util/AdobeUtilityTest.java
+++ src/test/java/com/imagination/cbs/util/AdobeUtilityTest.java
@@ -1,7 +1,9 @@
 package com.imagination.cbs.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -16,7 +18,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -35,6 +36,8 @@ import com.imagination.cbs.service.AdobeSignService;
 @RunWith(MockitoJUnitRunner.class)
 public class AdobeUtilityTest {
 
+	@InjectMocks
+	private AdobeUtility adobeUtility;
 	
 	@Mock
 	private ConfigRepository configRepository;
@@ -45,10 +48,8 @@ public class AdobeUtilityTest {
 	@Mock
 	private AdobeSignService adobeSignService;
 	
-	@InjectMocks
-	private AdobeUtility adobeUtility;
-	
 	JsonNode jsonNodeObj;
+	
 	@Before
 	public void init() throws JsonMappingException, JsonProcessingException {
 		String jsonString = "{\"access_token\":\"3AAABLblqZhCuY1hFaBq5aUHda\","
@@ -64,17 +65,17 @@ public class AdobeUtilityTest {
 	@Test
 	public void shouldReturnNewOauthAccessToken_getOauthAccessToken() {
 
-		ResponseEntity<JsonNode> mockedResult = Mockito.mock(ResponseEntity.class);
+		ResponseEntity<JsonNode> mockedResult = mock(ResponseEntity.class);
 		
 		when(configRepository.findBykeyNameStartingWith("ADOBE")).thenReturn(getConfigList("Empty", true, false));
-		when(restTemplate.exchange(eq("/token"), eq(HttpMethod.POST), Mockito.any(), eq(JsonNode.class))).thenReturn(mockedResult);
+		when(restTemplate.exchange(eq("/token"), eq(HttpMethod.POST), any(), eq(JsonNode.class))).thenReturn(mockedResult);
 		when(mockedResult.getBody()).thenReturn(jsonNodeObj);
 		when(mockedResult.getStatusCode()).thenReturn(HttpStatus.OK);
 		
 		String accessToken = adobeUtility.getOauthAccessToken();
 		
 		verify(configRepository, times(2)).findBykeyNameStartingWith("ADOBE");
-		verify(restTemplate).exchange(eq("/token"), eq(HttpMethod.POST), Mockito.any(), eq(JsonNode.class));
+		verify(restTemplate).exchange(eq("/token"), eq(HttpMethod.POST), any(), eq(JsonNode.class));
 		
 		assertEquals("Bearer 3AAABLblqZhCuY1hFaBq5aUHda", accessToken);
 	}
@@ -83,16 +84,16 @@ public class AdobeUtilityTest {
 	@Test
 	public void shouldReturnNewOauthAccessTokenWhenExistingTokenIsExpired_getOauthAccessToken() {
 
-		ResponseEntity<JsonNode> mockedResult = Mockito.mock(ResponseEntity.class);
+		ResponseEntity<JsonNode> mockedResult = mock(ResponseEntity.class);
 		
 		when(configRepository.findBykeyNameStartingWith("ADOBE")).thenReturn(getConfigList("ADOBE", true, true));
-		when(restTemplate.exchange(eq("https://secure.echosign.com/oauth/refresh"), eq(HttpMethod.POST), Mockito.any(), eq(JsonNode.class))).thenReturn(mockedResult);
+		when(restTemplate.exchange(eq("https://secure.echosign.com/oauth/refresh"), eq(HttpMethod.POST), any(), eq(JsonNode.class))).thenReturn(mockedResult);
 		when(mockedResult.getBody()).thenReturn(jsonNodeObj);
 		
 		String accessToken = adobeUtility.getOauthAccessToken();
 		
 		verify(configRepository, times(3)).findBykeyNameStartingWith("ADOBE");
-		verify(restTemplate).exchange(eq("https://secure.echosign.com/oauth/refresh"), eq(HttpMethod.POST), Mockito.any(), eq(JsonNode.class));
+		verify(restTemplate).exchange(eq("https://secure.echosign.com/oauth/refresh"), eq(HttpMethod.POST), any(), eq(JsonNode.class));
 		
 		assertEquals("Bearer 3AAABLblqZhCuY1hFaBq5aUHda", accessToken);
 	}
@@ -113,16 +114,16 @@ public class AdobeUtilityTest {
 	@Test
 	public void shouldReturnOauthAccessTokenWhenAcessTokenIsEmpty_getOauthAccessToken() {
 
-		ResponseEntity<JsonNode> mockedResult = Mockito.mock(ResponseEntity.class);
+		ResponseEntity<JsonNode> mockedResult = mock(ResponseEntity.class);
 	
 		when(configRepository.findBykeyNameStartingWith("ADOBE")).thenReturn(getConfigList("ADOBE", false, true));
-		when(restTemplate.exchange(eq("https://secure.echosign.com/oauth/refresh"), eq(HttpMethod.POST), Mockito.any(), eq(JsonNode.class))).thenReturn(mockedResult);
+		when(restTemplate.exchange(eq("https://secure.echosign.com/oauth/refresh"), eq(HttpMethod.POST), any(), eq(JsonNode.class))).thenReturn(mockedResult);
 		when(mockedResult.getBody()).thenReturn(jsonNodeObj);
 		
 		String accessToken = adobeUtility.getOauthAccessToken();
 
 		verify(configRepository, times(3)).findBykeyNameStartingWith("ADOBE");
-		verify(restTemplate).exchange(eq("https://secure.echosign.com/oauth/refresh"), eq(HttpMethod.POST), Mockito.any(), eq(JsonNode.class));
+		verify(restTemplate).exchange(eq("https://secure.echosign.com/oauth/refresh"), eq(HttpMethod.POST), any(), eq(JsonNode.class));
 
 		assertEquals("Bearer 3AAABLblqZhCuY1hFaBq5aUHda", accessToken);
 	}
@@ -141,24 +142,24 @@ public class AdobeUtilityTest {
 	public void shouldThrowCBSApplicationExceptionWhenRuntimeExceptionOccured_getNewAccessToken() {
 		
 		when(configRepository.findBykeyNameStartingWith("ADOBE")).thenReturn(getConfigList("Empty", true, false));
-		when(restTemplate.exchange(eq("/token"), eq(HttpMethod.POST), Mockito.any(), eq(JsonNode.class))).thenThrow(RuntimeException.class);
+		when(restTemplate.exchange(eq("/token"), eq(HttpMethod.POST), any(), eq(JsonNode.class))).thenThrow(RuntimeException.class);
 		
 		adobeUtility.getOauthAccessToken();
 		
 		verify(configRepository).findBykeyNameStartingWith("ADOBE");
-		verify(restTemplate).exchange(eq("/token"), eq(HttpMethod.POST), Mockito.any(), eq(JsonNode.class));
+		verify(restTemplate).exchange(eq("/token"), eq(HttpMethod.POST), any(), eq(JsonNode.class));
 	}
 	
 	@Test
 	public void expectNullTokenWhileExceptionInGetOauthAccessTokenFromRefreshToken() {
 		
 		when(configRepository.findBykeyNameStartingWith("ADOBE")).thenReturn(getConfigList("ADOBE", true, true));
-		when(restTemplate.exchange(eq("https://secure.echosign.com/oauth/refresh"), eq(HttpMethod.POST), Mockito.any(), eq(JsonNode.class))).thenThrow(RuntimeException.class);
+		when(restTemplate.exchange(eq("https://secure.echosign.com/oauth/refresh"), eq(HttpMethod.POST), any(), eq(JsonNode.class))).thenThrow(RuntimeException.class);
 
 		String accessToken = adobeUtility.getOauthAccessToken();
 
 		verify(configRepository, times(3)).findBykeyNameStartingWith("ADOBE");
-		verify(restTemplate).exchange(eq("https://secure.echosign.com/oauth/refresh"), eq(HttpMethod.POST), Mockito.any(), eq(JsonNode.class));
+		verify(restTemplate).exchange(eq("https://secure.echosign.com/oauth/refresh"), eq(HttpMethod.POST), any(), eq(JsonNode.class));
 
 		assertEquals("Bearer null",accessToken);
 	}
@@ -166,18 +167,18 @@ public class AdobeUtilityTest {
 	@SuppressWarnings("unchecked")
 	@Test
 	public void expectNullTokenWhileExceptionInConvertJsonToObj() {
-		ResponseEntity<JsonNode> mockedResult = Mockito.mock(ResponseEntity.class);
-		JsonNode jsonNode = Mockito.mock(JsonNode.class);
+		ResponseEntity<JsonNode> mockedResult = mock(ResponseEntity.class);
+		JsonNode jsonNode = mock(JsonNode.class);
 		
 		when(configRepository.findBykeyNameStartingWith("ADOBE")).thenReturn(getConfigList("ADOBE", true, true));
-		when(restTemplate.exchange(eq("https://secure.echosign.com/oauth/refresh"), eq(HttpMethod.POST), Mockito.any(), eq(JsonNode.class))).thenReturn(mockedResult);
+		when(restTemplate.exchange(eq("https://secure.echosign.com/oauth/refresh"), eq(HttpMethod.POST), any(), eq(JsonNode.class))).thenReturn(mockedResult);
 		when(mockedResult.getBody()).thenReturn(jsonNode);
 		when(jsonNode.path("access_token")).thenThrow(RuntimeException.class);
 		
 		String accessToken = adobeUtility.getOauthAccessToken();
 
 		verify(configRepository, times(3)).findBykeyNameStartingWith("ADOBE");
-		verify(restTemplate).exchange(eq("https://secure.echosign.com/oauth/refresh"), eq(HttpMethod.POST), Mockito.any(), eq(JsonNode.class));
+		verify(restTemplate).exchange(eq("https://secure.echosign.com/oauth/refresh"), eq(HttpMethod.POST), any(), eq(JsonNode.class));
 
 		assertEquals("Bearer null",accessToken);
 	}
@@ -197,11 +198,11 @@ public class AdobeUtilityTest {
 	@SuppressWarnings("unchecked")
 	@Test
 	public void shouldGetNewBaseURIForRestAPI() {
-		ResponseEntity<JsonNode> mockedResult = Mockito.mock(ResponseEntity.class);
-		JsonNode jsonNode = Mockito.mock(JsonNode.class);
+		ResponseEntity<JsonNode> mockedResult = mock(ResponseEntity.class);
+		JsonNode jsonNode = mock(JsonNode.class);
 
 		when(configRepository.findBykeyNameStartingWith("ADOBE_API_BASE_URI")).thenReturn(getConfigList("Empty", true, true));
-		when(restTemplate.exchange(eq(""), eq(HttpMethod.GET), Mockito.any(), eq(JsonNode.class))).thenReturn(mockedResult);
+		when(restTemplate.exchange(eq(""), eq(HttpMethod.GET), any(), eq(JsonNode.class))).thenReturn(mockedResult);
 		when(mockedResult.getBody()).thenReturn(jsonNode);
 		when(jsonNode.path("apiAccessPoint")).thenReturn(jsonNode);
 		when(jsonNode.asText()).thenReturn("localhost:8080/baseUrl/");
@@ -209,7 +210,7 @@ public class AdobeUtilityTest {
 		String baseUrl = adobeUtility.getBaseURIForRestAPI("3AAABLblqZhCuY1hFaBq5aUHda_kQrZBWfpVAXiSAo");
 		
 		verify(configRepository).findBykeyNameStartingWith("ADOBE_API_BASE_URI");
-		verify(restTemplate).exchange(eq(""), eq(HttpMethod.GET), Mockito.any(), eq(JsonNode.class));
+		verify(restTemplate).exchange(eq(""), eq(HttpMethod.GET), any(), eq(JsonNode.class));
 		
 		assertEquals("localhost:8080/baseUrl/api/rest/v6", baseUrl);
 	}
