a9256a2b4ceed293f7deef518b939ca8a46fdfb1
PappuRout <pappuroutvtu@gmail.com>
2020-04-27 12:03:21 +0530

    Reafctor code.

--- src/main/java/com/imagination/cbs/service/MaconomyService.java
+++ src/main/java/com/imagination/cbs/service/MaconomyService.java
@@ -3,12 +3,14 @@
  */
 package com.imagination.cbs.service;
 
+import java.io.IOException;
+
 /**
  * @author pappu.rout
  *
  */
 public interface MaconomyService {
 	
-	public <T> T getMaconomyJobNumberAndDepartmentsDetails(String jobNumber, T jobDataDto, String isJobNumberOrDepartmentName, String departname);
+	public <T> T getMaconomyJobNumberAndDepartmentsDetails(String jobNumber, T jobDataDto, String isJobNumberOrDepartmentName, String departname) throws IOException;
 
 }
