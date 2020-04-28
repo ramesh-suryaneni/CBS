487fc9f9cde13578b9297acb39a70de843f1314d
Akshay Dnyaneshwar Hande <akshay.hande@yash.com>
2020-04-28 19:45:53 +0530

    Changes according to suggestions

--- src/test/java/com/imagination/cbs/util/MaconomyRestClientTest.java
+++ src/test/java/com/imagination/cbs/util/MaconomyRestClientTest.java
@@ -1,7 +1,6 @@
 package com.imagination.cbs.util;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
@@ -30,23 +29,33 @@ public class MaconomyRestClientTest {
     private RestTemplate restTemplate;
 
  	@Test
- 	public void shouldCallRestServiceForGet() {
+ 	public void callRestServiceForGet_shouldReturnSuccess_forGetRequest() {
  		
  		when(restTemplate.exchange(eq("https:baseurl/maconomy/rest"), eq(HttpMethod.GET), any(), eq(JsonNode.class))).thenReturn(new ResponseEntity<>(HttpStatus.OK));
  		
- 		ResponseEntity<JsonNode> actual = maconomyRestClient.callRestServiceForGet("https:baseurl/maconomy/rest", "username", "password");
+ 		final ResponseEntity<JsonNode> actual = maconomyRestClient.callRestServiceForGet("https:baseurl/maconomy/rest", "username", "password");
  		
  		verify(restTemplate).exchange(eq("https:baseurl/maconomy/rest"), eq(HttpMethod.GET), any(), eq(JsonNode.class));
 
- 		assertNotNull(actual);
  		assertEquals(HttpStatus.OK, actual.getStatusCode());
  	}
 
+ 	@Test
+ 	public void callRestServiceForGet_shouldReturnFailure_forGetRequest() {
+ 		
+ 		when(restTemplate.exchange(eq("https:baseurl/maconomy/rest"), eq(HttpMethod.GET), any(), eq(JsonNode.class))).thenReturn(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+ 		
+ 		final ResponseEntity<JsonNode> actual = maconomyRestClient.callRestServiceForGet("https:baseurl/maconomy/rest", "username", "password");
+ 		
+ 		verify(restTemplate).exchange(eq("https:baseurl/maconomy/rest"), eq(HttpMethod.GET), any(), eq(JsonNode.class));
+
+ 		assertEquals(HttpStatus.NOT_FOUND, actual.getStatusCode());
+ 	}
+
  	@Test(expected = ResourceNotFoundException.class)
- 	public void shouldThrowResourceNotFoundExceptionWhenRuntimeExceptionOccured() {
+ 	public void callRestServiceForGet_shouldThrowResourceNotFoundException_whenRuntimeExceptionOccured() {
 
  		when(restTemplate.exchange(eq("https:baseurl/maconomy/rest"), eq(HttpMethod.GET), any(), eq(JsonNode.class))).thenThrow(RuntimeException.class);
- 		
  		maconomyRestClient.callRestServiceForGet("https:baseurl/maconomy/rest", "username", "password");
  	}
 }
